7d87b05676f349e6f71323756a35fb2635a58e45
Gabriel Blanco <blancgab@gmail.com>
2015-03-09 19:22:09 -0400

    aesthetic changes

--- hello.c
+++ hello.c
@@ -21,71 +21,71 @@ int vga_led_fd;
 
 int main()
 {
-  vga_led_arg_t vla;
-  int i;
-  static const char filename[] = "/dev/vga_led";
-
-  int x_pos, x_vel, y_pos, y_vel;
-
-  /* Initial Position and Velocity Vectors */
-  x_pos = 666;
-  y_pos = 269;
-  x_vel = 1;
-  y_vel = 1;
-
-  printf("VGA LED Userspace program started\n");
-
-  if ((vga_led_fd = open(filename, O_RDWR)) == -1) 
-  {
-    fprintf(stderr, "could not open %s\n", filename);
-    return -1;
-  }
-
-  while (1)
-  {
-      x_pos = x_pos + x_vel;
-      y_pos = y_pos + y_vel;
-      
-      if (x_pos > X_MAX)
-      {
-	  x_pos = X_MAX;
-	  x_vel = -x_vel;
-      }
-      else if (x_pos < X_MIN)
-      {
-	  x_pos = X_MIN;
-	  x_vel = -x_vel;
-      }
-      
-      if (y_pos > Y_MAX)
-      {
-	  y_pos = Y_MAX;
-	  y_vel = -y_vel;
-      }
-      else if (y_pos < Y_MIN)
-      {
-	  y_pos = Y_MIN;
-	  y_vel = -y_vel;
-      }
-
-      write_position(x_pos, y_pos);
-      usleep(30000);    
-  }  
-
-  printf("VGA LED Userspace program terminating\n");
-  return 0;
+    vga_led_arg_t vla;
+    int i;
+    static const char filename[] = "/dev/vga_led";
+    
+    int x_pos, x_vel, y_pos, y_vel;
+    
+    /* Initial Position and Velocity Vectors */
+    x_pos = 666;
+    y_pos = 269;
+    x_vel = 1;
+    y_vel = 1;
+    
+    printf("VGA LED Userspace program started\n");
+    
+    if ((vga_led_fd = open(filename, O_RDWR)) == -1) 
+    {
+	fprintf(stderr, "could not open %s\n", filename);
+	return -1;
+    }
+    
+    while (1)
+    {
+	x_pos = x_pos + x_vel;
+	y_pos = y_pos + y_vel;
+	
+	if (x_pos > X_MAX)
+	{
+	    x_pos = X_MAX;
+	    x_vel = -x_vel;
+	}
+	else if (x_pos < X_MIN)
+	{
+	    x_pos = X_MIN;
+	    x_vel = -x_vel;
+	}
+	
+	if (y_pos > Y_MAX)
+	{
+	    y_pos = Y_MAX;
+	    y_vel = -y_vel;
+	}
+	else if (y_pos < Y_MIN)
+	{
+	    y_pos = Y_MIN;
+	    y_vel = -y_vel;
+	}
+	
+	write_position(x_pos, y_pos);
+	usleep(30000);    
+    }  
+    
+    printf("VGA LED Userspace program terminating\n");
+    return 0;
 }
 
 void write_position(int x, int y)
 {
-  vga_led_arg_t vla;  
-  printf("BALL POSITION (%d, %d)\n", x, y);
-  vla.x = (unsigned int) x;
-  vla.y = (unsigned int) y;
-
-  if (ioctl(vga_led_fd, VGA_LED_DRAW_BALL, &vla))
-  {
-    perror("ioctl(VGA_LED_DRAW_BALL) failed");
-    return;
-  }
+    vga_led_arg_t vla;  
+    printf("BALL POSITION (%d, %d)\n", x, y);
+    vla.x = (unsigned int) x;
+    vla.y = (unsigned int) y;
+    
+    if (ioctl(vga_led_fd, VGA_LED_DRAW_BALL, &vla))
+    {
+	perror("ioctl(VGA_LED_DRAW_BALL) failed");
+	return;
+    }
 }
--- vga_led.c
+++ vga_led.c
@@ -54,7 +54,7 @@ static void write_coordinates(int x, int y)
 {  
     iowrite16((u16)x, dev.virtbase);
     iowrite16((u16)y, dev.virtbase + 2);
-
+    
     dev.coordinates[0] = x;
     dev.coordinates[1] = y;
 }
@@ -86,15 +86,15 @@ vga_led_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 
 /* The operations our device knows how to do */
 static const struct file_operations vga_led_fops = {
-	.owner		= THIS_MODULE,
-	.unlocked_ioctl = vga_led_ioctl,
+    .owner		= THIS_MODULE,
+    .unlocked_ioctl     = vga_led_ioctl,
 };
 
 /* Information about our device for the "misc" framework -- like a char dev */
 static struct miscdevice vga_led_misc_device = {
-	.minor		= MISC_DYNAMIC_MINOR,
-	.name		= DRIVER_NAME,
-	.fops		= &vga_led_fops,
+    .minor		= MISC_DYNAMIC_MINOR,
+    .name		= DRIVER_NAME,
+    .fops		= &vga_led_fops,
 };
 
 /*
@@ -103,84 +103,89 @@ static struct miscdevice vga_led_misc_device = {
  */
 static int __init vga_led_probe(struct platform_device *pdev)
 {
-	int ret;
-
-	/* Register ourselves as a misc device: creates /dev/vga_led */
-	ret = misc_register(&vga_led_misc_device);
-
-	/* Get the address of our registers from the device tree */
-	ret = of_address_to_resource(pdev->dev.of_node, 0, &dev.res);
-	if (ret) {
-		ret = -ENOENT;
-		goto out_deregister;
-	}
-
-	/* Make sure we can use these registers */
-	if (request_mem_region(dev.res.start, resource_size(&dev.res),
-			       DRIVER_NAME) == NULL) {
-		ret = -EBUSY;
-		goto out_deregister;
-	}
-
-	/* Arrange access to our registers */
-	dev.virtbase = of_iomap(pdev->dev.of_node, 0);
-	if (dev.virtbase == NULL) {
-		ret = -ENOMEM;
-		goto out_release_mem_region;
-	}
-
-	/* Plot Static Ball*/
-	write_coordinates(200,200);
-	  
-	return 0;
+    int ret;
+    
+    /* Register ourselves as a misc device: creates /dev/vga_led */
+    ret = misc_register(&vga_led_misc_device);
+    
+    /* Get the address of our registers from the device tree */
+    ret = of_address_to_resource(pdev->dev.of_node, 0, &dev.res);
+    if (ret) 
+    {
+	ret = -ENOENT;
+	goto out_deregister;
+    }
+    
+    /* Make sure we can use these registers */
+    if (request_mem_region(dev.res.start, resource_size(&dev.res), DRIVER_NAME) == NULL) 
+    {
+	ret = -EBUSY;
+	goto out_deregister;
+    }
+    
+    /* Arrange access to our registers */
+    dev.virtbase = of_iomap(pdev->dev.of_node, 0);
+    if (dev.virtbase == NULL) 
+    {
+	ret = -ENOMEM;
+	goto out_release_mem_region;
+    }
+    
+    /* Plot Static Ball*/
+    write_coordinates(200,200);
+    
+    return 0;
 
 out_release_mem_region:
-	release_mem_region(dev.res.start, resource_size(&dev.res));
+    release_mem_region(dev.res.start, resource_size(&dev.res));
 out_deregister:
-	misc_deregister(&vga_led_misc_device);
-	return ret;
+    misc_deregister(&vga_led_misc_device);
+    return ret;
 }
 
 /* Clean-up code: release resources */
 static int vga_led_remove(struct platform_device *pdev)
 {
-	iounmap(dev.virtbase);
-	release_mem_region(dev.res.start, resource_size(&dev.res));
-	misc_deregister(&vga_led_misc_device);
-	return 0;
+    iounmap(dev.virtbase);
+    release_mem_region(dev.res.start, resource_size(&dev.res));
+    misc_deregister(&vga_led_misc_device);
+    return 0;
 }
 
 /* Which "compatible" string(s) to search for in the Device Tree */
 #ifdef CONFIG_OF
-static const struct of_device_id vga_led_of_match[] = {
-	{ .compatible = "altr,vga_led" },
-	{},
+static const struct of_device_id vga_led_of_match[] = 
+{
+    { .compatible = "altr,vga_led" },
+    {},
 };
 MODULE_DEVICE_TABLE(of, vga_led_of_match);
 #endif
 
 /* Information for registering ourselves as a "platform" driver */
-static struct platform_driver vga_led_driver = {
-	.driver	= {
-		.name	= DRIVER_NAME,
-		.owner	= THIS_MODULE,
-		.of_match_table = of_match_ptr(vga_led_of_match),
-	},
-	.remove	= __exit_p(vga_led_remove),
+static struct platform_driver vga_led_driver = 
+{
+    .driver = 
+    {
+	.name	        = DRIVER_NAME,
+	.owner	        = THIS_MODULE,
+	.of_match_table = of_match_ptr(vga_led_of_match),
+    },
+    .remove      	= __exit_p(vga_led_remove),
 };
 
 /* Called when the module is loaded: set things up */
 static int __init vga_led_init(void)
 {
-	pr_info(DRIVER_NAME ": init\n");
-	return platform_driver_probe(&vga_led_driver, vga_led_probe);
+    pr_info(DRIVER_NAME ": init\n");
+    return platform_driver_probe(&vga_led_driver, vga_led_probe);
 }
 
 /* Called when the module is unloaded: release resources */
 static void __exit vga_led_exit(void)
 {
-	platform_driver_unregister(&vga_led_driver);
-	pr_info(DRIVER_NAME ": exit\n");
+    platform_driver_unregister(&vga_led_driver);
+    pr_info(DRIVER_NAME ": exit\n");
 }
 
 module_init(vga_led_init);
